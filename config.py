b803d1442d710b4d18c6b14368c967bc6ab36629
Standard System <standardvyang@gmail.com>
2024-04-17 15:33:24 -0600

    project 2

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
 import os
 
-name_of_book = "whitenights"
-full_book_txt = "whitenights_full.txt"
+name_of_book = "thegambler"
+full_book_txt = "thegambler_full.txt"
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 model_to_use = "gpt-3.5-turbo-0125"
 backup_model = "gpt-4-turbo"
